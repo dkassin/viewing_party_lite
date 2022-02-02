84def415b0ceaca6ba61d626d9c7b6642f32494e
David Kassin <david.j.kassin@gmail.com>
2022-02-02 08:22:29 -0700

    Add params to response for api

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,15 +1,15 @@
 class MoviesController < ApplicationController
-  def index
-    conn = Faraday.new("https://api.themoviedb.org") do |faraday|
-      faraday.params['api_key'] = ENV['movie_api_key']
-    end
-
-    response = conn.get("/3/search/movie?query=Aliens&api_key=6bb70d8448dabf0d4dbdfa9e215e1826")
-    data = JSON.parse(response.body, symbolize_names: true)
-
-    @movies = data[:results]
-
-  end
+  # def index
+  #   conn = Faraday.new("https://api.themoviedb.org") do |faraday|
+  #     faraday.params['api_key'] = ENV['movie_api_key']
+  #   end
+  #
+  #   response = conn.get("/3/search/movie?query=Aliens&api_key=6bb70d8448dabf0d4dbdfa9e215e1826")
+  #   data = JSON.parse(response.body, symbolize_names: true)
+  #
+  #   @movies = data[:results]
+  #
+  # end
 
   def search
     @user = User.find(params[:id])
@@ -17,14 +17,14 @@ class MoviesController < ApplicationController
       faraday.params['api_key'] = ENV['movie_api_key']
     end
 
-    response = conn.get("/3/search/movie?query=Aliens&api_key=6bb70d8448dabf0d4dbdfa9e215e1826")
+    response = conn.get("/3/search/movie?query=#{params[:search]}&api_key=6bb70d8448dabf0d4dbdfa9e215e1826")
     data = JSON.parse(response.body, symbolize_names: true)
 
     movie = data[:results]
 
     @found_movie = movie.find {|m| m[:original_title] == params[:search]}
     @movie = @found_movie[:original_title]
-
+    binding.pry
     render "/users/#{@user.id}/discover"
   end
 end
