53dc49a2af8d2ef4b5293f4b011ff9a236567a23
David Kassin <david.j.kassin@gmail.com>
2022-02-02 10:29:36 -0700

    Test: Add test for discover page button path

--- spec/features/search_for_movie_spec.rb
+++ spec/features/search_for_movie_spec.rb
@@ -12,8 +12,21 @@ RSpec.describe 'Movie Search' do
 
       fill_in :search, with: 'Aliens'
       click_button 'Search'
-      expect(page.status_code).to eq 204
-      expect(page).to have_content("Aliens")
+      expect(page.status_code).to eq 200
+      expect(page).to have_content("Cowboys & Aliens")
+
+      expect(page).to have_content("Vote Average: 5.5")
+    end
+
+    it 'goes back to discover page after filling in form when clicking button' do
+      visit "/users/#{@user_1.id}/discover"
+
+      fill_in :search, with: 'Aliens'
+      click_button 'Search'
+      expect(page.status_code).to eq 200
+      save_and_open_page
+      click_button 'Discover Page'
+      expect(current_path).to eq("/users/#{@user_1.id}/discover")
     end
   end
 end
