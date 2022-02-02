5723ccb82aab2a2cac44503b4fe8fe0bc0b5ef39
Robin Dittrich <robinodittrich@gmail.com>
2022-02-02 09:43:58 -0800

    Create user movies controller.

---
+++ app/controllers/user_movies_controller.rb
@@ -0,0 +1,14 @@
+class UserMoviesController < ApplicationController
+  def index
+    conn = Faraday.new("https://api.themoviedb.org") do |faraday|
+      faraday.params['api_key'] = ENV['movie_api_key']
+    end
+    response_page_one = conn.get("/3/movie/top_rated?api_key=6bb70d8448dabf0d4dbdfa9e215e1826")
+    response_page_two = conn.get("/3/movie/top_rated?page=2&api_key=6bb70d8448dabf0d4dbdfa9e215e1826")
+    data_page_one = JSON.parse(response_page_one.body, symbolize_names: true)
+    data_page_two = JSON.parse(response_page_two.body, symbolize_names: true)
+    a = data_page_one[:results]
+    b = data_page_two[:results]
+    @movies = (a << b).flatten
+  end
+end
