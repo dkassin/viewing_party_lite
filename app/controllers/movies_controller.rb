e71033614b70f5cfc6935f9e5e5f6cae0a669870
David Kassin <david.j.kassin@gmail.com>
2022-02-01 20:39:30 -0700

    Need to work on views for api pull, controller api pull working

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,30 @@
 class MoviesController < ApplicationController
-  def search
-
-    conn = Faraday.get("https://api.themoviedb.org") do |faraday|
+  def index
+    conn = Faraday.new("https://api.themoviedb.org") do |faraday|
       faraday.params['api_key'] = ENV['movie_api_key']
     end
-    
+
     response = conn.get("/3/search/movie?query=Aliens&api_key=6bb70d8448dabf0d4dbdfa9e215e1826")
-    binding.pry
-    ata = JSON.parse(response.body, symbolize_names: true)
-    @movie = data[:results][0][:original_title]
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    @movies = data[:results]
+
+  end
+
+  def search
+    @user = User.find(params[:id])
+    conn = Faraday.new("https://api.themoviedb.org") do |faraday|
+      faraday.params['api_key'] = ENV['movie_api_key']
+    end
+
+    response = conn.get("/3/search/movie?query=Aliens&api_key=6bb70d8448dabf0d4dbdfa9e215e1826")
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    movie = data[:results]
+
+    @found_movie = movie.find {|m| m[:original_title] == params[:search]}
+    @movie = @found_movie[:original_title]
+
+    render "/users/#{@user.id}/discover"
   end
 end
