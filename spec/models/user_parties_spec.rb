585978952c71b046372dc6f9821f18ed36d4148d
Robin Dittrich <robinodittrich@gmail.com>
2022-02-04 10:38:15 -0800

    Refactor and create more robust validation tests.

--- spec/models/user_parties_spec.rb
+++ spec/models/user_parties_spec.rb
@@ -6,7 +6,19 @@ RSpec.describe UserParty, type: :model do
     it {should belong_to :party}
   end
   describe 'validations' do
-    it {should validate_presence_of :user_id}
-    it {should validate_presence_of :party_id}
+    user = User.new(name: "Jim", email: "Jim@email.com")
+    party = user.parties.new(duration: 180, day: "December 12, 2021", start_time: "7:00 pm", movie_id: 1)
+
+    subject { user.user_parties.new(user_id: user.id, party_id: party.id) }
+
+    describe '#user_id' do
+      it {should validate_presence_of :user_id}
+      it { should_not allow_value(nil).for(:user_id) }
+    end
+
+    describe '#party_id' do
+      it {should validate_presence_of :party_id}
+      it { should_not allow_value(nil).for(:party_id) }
+    end
   end
 end
