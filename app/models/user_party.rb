47c38a6955a6d686c83f25110c9c04d21af4f435
Robin Dittrich <robinodittrich@gmail.com>
2022-02-04 10:39:02 -0800

    Refactor and create more robust validation tests.

--- app/models/user_party.rb
+++ app/models/user_party.rb
@@ -2,6 +2,8 @@ class UserParty < ApplicationRecord
   belongs_to :user
   belongs_to :party
 
-  validates_presence_of :user_id
-  validates_presence_of :party_id
+  validates :user_id,
+            :presence => {message: "can't be blank"}
+  validates :party_id,
+            :presence => {message: "can't be blank"}
 end
