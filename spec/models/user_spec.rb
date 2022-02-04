68821a8541722c30c95bcc8a3666e18206295653
Robin Dittrich <robinodittrich@gmail.com>
2022-02-04 10:38:11 -0800

    Refactor and create more robust validation tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,8 +5,18 @@ RSpec.describe User, type: :model do
     it { should have_many :user_parties}
     it { should have_many(:parties).through(:user_parties) }
   end
+
   describe 'validations' do
-    it {should validate_presence_of :email }
-    it {should validate_presence_of :name }
+    subject { User.new(name: "Jim", email: "Jim@email.com") }
+
+    describe '#name' do
+      it {should validate_presence_of :name}
+      it { should_not allow_value(nil).for(:name) }
+    end
+
+    describe '#email' do
+      it {should validate_presence_of :email}
+      it { should_not allow_value(nil).for(:email) }
+    end
   end
 end
