fe3032d6e2011b0d537fd7d8083d95cbe17d937b
David Kassin <david.j.kassin@gmail.com>
2022-02-16 15:17:48 -0700

    setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ApplicationRecord
   has_many :user_parties
   has_many :parties, through: :user_parties
+  has_secure_password
 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP },
             :presence => {message: "can't be blank"},
             :uniqueness => true
   validates :name,
             :presence => {message: "can't be blank"}
-
+  validates_presence_of :password_digest
 
   def self.except_self(user)
     where.not(id: user.id)
