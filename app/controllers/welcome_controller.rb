003900ff17836eb7dd991b35b409bd480be68f98
David Kassin <david.j.kassin@gmail.com>
2022-02-16 16:14:52 -0700

    completed authentication

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,17 +9,16 @@ class WelcomeController < ApplicationController
   def create
     user = User.create(user_params)
     if user.save
+      flash[:success] = "Welcome, #{user.name}!"
       redirect_to user_path(user.id)
     else
       flash[:alert] = "Error: Name can't be blank, Email can't be blank and must be valid."
       redirect_to "/register"
     end
-
-
   end
 
   private
     def user_params
-      params.permit(:name, :email)
+      params.permit(:name, :email, :password, :password_confirmation)
     end
 end
